5fadeef78200a7689257dd58ccb242d5769f605a
Xuanli Lin <kazumi@asu.edu>
2024-04-04 14:10:51 -0700

    gurobi sample code

--- opt_test.py
+++ opt_test.py
@@ -1,27 +1,54 @@
+import math
+
 from gurobipy import *
+
 
 # Create a new model
 m = Model()
 
-# Create variables
-x1 = m.addVar(name="x1")
-x2 = m.addVar(name="x2")
 
-# Set objective function
-m.setObjective(3*x1 + 2*x2 , GRB.MAXIMIZE)
+# VARIABLE and VAR-FUNCTION DECLARATIONS
+# W^+_i(r)
+def w_plus(i, r):
+    pass
 
-#Add constraints
-m.addConstr(2.2*x1 + x2  <= 100, "c1")
-m.addConstr(x2  <= 30, "c3")
-m.addConstr(x1  >= 0, "c4")
-m.addConstr(x2  >= 0, "c5")
+
+# R^{+,q}_i(r)
+def r_plus(i, q, r):
+    pass
+
+
+def alpha(i):
+    pass
+
+
+def phi(q, i):
+    pass
+
+
+delta = m.addVar(name='delta')
+beta = 0.95
+p = []
+d = []
+
+num_commodity = 10
+
+
+# CONSTRAINTS
+# Eq. 39
+for i in range(num_commodity):
+    temp = 0
+    for q in range(0, int(math.pow(2, 4))):
+        temp += p[q] * phi(q, i)
+    m.addConstr(d[i] - alpha(i) + (1/beta) * temp <= delta)
+
 
 # Optimize model
 m.optimize()
 
-#Print values for decision variables
+# Print values for decision variables
 for v in m.getVars():
     print(v.varName, v.x)
 
-#Print maximized profit value
-print('Maximized profit:',  m.objVal)
+# Print maximized profit value
+print('Maximized profit:', m.objVal)
