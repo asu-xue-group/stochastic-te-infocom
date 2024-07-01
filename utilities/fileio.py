5efc390da8b030cba384eb8cda7d16f1bd460cf3
Xuanli Lin <kaz@kazumi.blog>
2024-06-30 18:58:29 -0700

    added file to read reduced flows

--- utilities/fileio.py
+++ utilities/fileio.py
@@ -5,10 +5,24 @@ def read_W(G: DiGraph, I, file):
     W = {}
     for edge in G.edges:
         for i in I:
-            W[i, *edge] = 0
+            W[i, edge[0], edge[1]] = 0
     with open(file, 'r') as f:
         for line in f:
             tokens = line.strip().split(',')
             W[int(tokens[0]), int(tokens[1]), int(tokens[2])] = float(tokens[3])
 
     return W
+
+
+def read_R(G: DiGraph, I, Q, file):
+    R = {}
+    for edge in G.edges:
+        for i in I:
+            for q in Q:
+                R[i, q, edge[0], edge[1]] = 0
+    with open(file, 'r') as f:
+        for line in f:
+            tokens = line.strip().split(',')
+            R[int(tokens[0]), int(tokens[1]), int(tokens[2]), int(tokens[3])] = float(tokens[4])
+
+    return R
